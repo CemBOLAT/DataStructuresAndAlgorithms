b8b489621858e5c3466fb27e9b2d469eabe5552b
Cemal Bolat <c.bolat@gtu.edu.tr>
2024-01-28 20:17:27 +0300

    finish

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"myContainerPackage","l":"JavaContainer"},{"p":"myContainerPackage","l":"JavaSet"},{"p":"myContainerPackage","l":"JavaVector"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"myContainerPackage","l":"JavaContainer"},{"p":"myContainerPackage","l":"JavaSet"},{"p":"myContainerPackage","l":"JavaVector"}]
