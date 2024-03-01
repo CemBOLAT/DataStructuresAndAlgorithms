0ecf5902c81c78caccfabf24fdfc7723df59c3e5
CemBOLAT <cemalbolat2003@outlook.com>
2024-03-01 15:34:35 +0300

    javachw1 only test left

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"myContainerPackage","l":"JavaContainer"},{"p":"myContainerPackage","l":"JavaSet"},{"p":"myContainerPackage","l":"JavaVector"}]
