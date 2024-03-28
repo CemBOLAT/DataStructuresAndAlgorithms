5b9ad4ca8706a99aa5ccdc0e432cadb0559c3d05
CemBOLAT <cemalbolat2003@outlook.com>
2024-03-28 03:53:31 +0300

    dying

--- DATAS/LINK-LIST/DoubleLinkedList/DoubleLinkedList.java
+++ DATAS/LINK-LIST/DoubleLinkedList/DoubleLinkedList.java
@@ -1,6 +1,6 @@
 import java.util.Iterator;
 
-public class DoubleLinkedList<E> {
+public class DoubleLinkedList<E> implements Iterable<E> {
     private Node<E> head;
     private Node<E> tail;
     private int size;
@@ -37,197 +37,199 @@ public class DoubleLinkedList<E> {
         return -1;
     }
 
+    public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
     public void add(E element) {
-        if (size == 0) {
-            Node<E> node = new Node<>(element);
-            head = node;
-            tail = node;
-        } else {
-            Node<E> node = new Node<>(element, null, tail);
-            tail.next = node;
-            tail = node;
-        }
-        size++;
+        addLast(element);
     }
 
-    public void add(int index, E element) throws IndexOutOfBoundsException {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("Out of Bounds");
-        }
+    public void add(int index, E element) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         if (index == 0) {
-            Node<E> node = new Node<>(element, head, null);
-            if (head != null)
-                head.prev = node;
-            head = node;
-            if (size == 0)
-                tail = node;
+            addFirst(element);
         } else if (index == size) {
-            Node<E> node = new Node<>(element, null, tail);
-            tail.next = node;
-            tail = node;
+            addLast(element);
         } else {
+            Node<E> newNode = new Node<>(element);
             Node<E> tmp = head;
-            for (int i = 0; i < index; i++) {
+            for (int i = 0; i < index - 1; i++) {
                 tmp = tmp.next;
             }
-            Node<E> node = new Node<>(element, tmp, tmp.prev);
-            tmp.prev.next = node;
-            tmp.prev = node;
+            newNode.next = tmp.next;
+            tmp.next.prev = newNode;
+            tmp.next = newNode;
+            newNode.prev = tmp;
+            size++;
         }
-        size++;
     }
 
-    public E remove(int index) throws IndexOutOfBoundsException {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Out of Bounds");
-        }
-        Node<E> nodeToRemove;
-        if (index == 0) {
-            nodeToRemove = head;
-            head = head.next;
-            if (head != null)
-                head.prev = null;
-            else
-                tail = null;
-        } else if (index == size - 1) {
-            nodeToRemove = tail;
-            tail = tail.prev;
-            tail.next = null;
-        } else {
-            Node<E> tmp = head;
-            for (int i = 0; i < index; i++) {
-                tmp = tmp.next;
-            }
-            nodeToRemove = tmp;
-            tmp.prev.next = tmp.next;
-            tmp.next.prev = tmp.prev;
-        }
+    public E removeFirst() {
+        if (isEmpty())
+            throw new IndexOutOfBoundsException("Empty list");
+        E data = head.data;
+        head = head.next;
+        if (head != null)
+            head.prev = null;
         size--;
-        return nodeToRemove.data;
+        return data;
     }
 
-    public E set(int index, E element) throws IndexOutOfBoundsException {
-        Node<E> tmp = getNodeAtIndex(index);
-        E oldValue = tmp.data;
-        tmp.data = element;
-        return oldValue;
+    public E removeLast() {
+        if (isEmpty())
+            throw new IndexOutOfBoundsException("Empty list");
+        E data = tail.data;
+        tail = tail.prev;
+        if (tail != null)
+            tail.next = null;
+        size--;
+        return data;
     }
 
-    public E get(int index) throws IndexOutOfBoundsException {
-        return getNodeAtIndex(index).data;
-    }
-
-    private Node<E> getNodeAtIndex(int index) throws IndexOutOfBoundsException {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException("Out of Bounds");
+    public E remove(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        if (index == 0) {
+            return removeFirst();
+        } else if (index == size - 1) {
+            return removeLast();
+        } else {
+            Node<E> tmp = head;
+            for (int i = 0; i < index - 1; i++) {
+                tmp = tmp.next;
+            }
+            E data = tmp.next.data;
+            tmp.next = tmp.next.next;
+            tmp.next.prev = tmp;
+            size--;
+            return data;
         }
+    }
+
+    public E getFirst() {
+        if (isEmpty())
+            throw new IndexOutOfBoundsException("Empty list");
+        return head.data;
+    }
+
+    public E getLast() {
+        if (isEmpty())
+            throw new IndexOutOfBoundsException("Empty list");
+        return tail.data;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         Node<E> tmp = head;
         for (int i = 0; i < index; i++) {
             tmp = tmp.next;
         }
-        return tmp;
+        return tmp.data;
+    }
+
+    public void set(int index, E element) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        Node<E> tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        tmp.data = element;
     }
 
     public int size() {
         return size;
     }
 
-    public Iterator<E> listIterator() {
-        return new ListIterator<>(head);
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
     }
 
-    public Iterator<E> listIterator(int index) {
-        return new ListIterator<>(getNodeAtIndex(index));
+    public Iterator<E> iterator() {
+        return new ListIterator();
     }
 
-    private static class Node<E> {
-        private E data;
-        private Node<E> next;
-        private Node<E> prev;
+    private class Node<E> {
+        E data;
+        Node<E> prev;
+        Node<E> next;
 
-        private Node(E data) {
-            this(data, null, null);
-        }
-
-        private Node(E data, Node<E> next, Node<E> prev) {
+        public Node(E data) {
             this.data = data;
-            this.next = next;
-            this.prev = prev;
+            this.prev = null;
+            this.next = null;
         }
     }
 
-    private class ListIterator<T> implements Iterator<E> {
-        private Node<E> node;
+    public class ListIterator implements Iterator<E> {
+        private Node<E> current;
 
-        public ListIterator(Node<E> start) {
-            node = start;
+        public ListIterator() {
+            current = head;
         }
 
         public boolean hasNext() {
-            return node != null;
+            return current != null;
         }
 
         public boolean hasPrevious() {
-            return node != null && node.prev != null;
+            return current != null;
         }
 
         public E next() {
-            if (!hasNext())
-                throw new IllegalStateException("No next element");
-            E value = node.data;
-            node = node.next;
-            return value;
+            E data = current.data;
+            current = current.next;
+            return data;
         }
 
         public E previous() {
-            if (!hasPrevious())
-                throw new IllegalStateException("No previous element");
-            if (node == null)
-                node = tail;
-            else
-                node = node.prev;
-            return node.data;
-        }
-
-        public void add(E obj) {
-            Node<E> newNode = new Node<>(obj);
-            if (node == null) { // Adding at the end
-                if (tail != null) {
-                    tail.next = newNode;
-                    newNode.prev = tail;
-                }
-                tail = newNode;
-                if (head == null) {
-                    head = newNode;
-                }
-            } else if (node.prev == null) { // Adding at the beginning
-                newNode.next = node;
-                node.prev = newNode;
-                head = newNode;
-            } else { // Adding in the middle
-                newNode.next = node;
-                newNode.prev = node.prev;
-                node.prev.next = newNode;
-                node.prev = newNode;
-            }
-            size++;
+            E data = current.data;
+            current = current.prev;
+            return data;
         }
 
         public void remove() {
-            if (node == null)
-                throw new IllegalStateException("No element to remove");
-            Node<E> prevNode = node.prev;
-            Node<E> nextNode = node.next;
-            if (prevNode != null)
-                prevNode.next = nextNode;
-            if (nextNode != null)
-                nextNode.prev = prevNode;
-            if (node == head)
-                head = nextNode;
-            if (node == tail)
-                tail = prevNode;
-            node = nextNode;
-            size--;
+            if (current == head) {
+                removeFirst();
+            } else if (current == tail) {
+                removeLast();
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+                size--;
+            }
+        }
+
+        public void set(E element) {
+            current.data = element;
         }
     }
+    
 }
--- DATAS/LINK-LIST/DoubleLinkedList/DoubleLinkedListTest.java
+++ DATAS/LINK-LIST/DoubleLinkedList/DoubleLinkedListTest.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 public class DoubleLinkedListTest {
     public static void main(String[] args) {
         // Creating a new double linked list
-        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<Integer>();
 
         // Adding elements to the list
         list.add(5);
@@ -13,68 +13,52 @@ public class DoubleLinkedListTest {
 
         // Displaying elements using Iterator
         System.out.println("Elements in the list:");
-        Iterator<Integer> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
         }
 
-        // Removing an element at index 2
-        list.remove(2);
-        System.out.println("\nAfter removing element at index 2:");
-        iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+        // Displaying elements in reverse order using ListIterator
+        System.out.println("Elements in the list in reverse order:");
+        DoubleLinkedList<Integer>.ListIterator lit = list.new ListIterator();
+        while (lit.hasPrevious()) {
+            System.out.println(lit.previous());
+        }
+        // Clearing the list
+        list.clear();
+        System.out.println("List is cleared.");
+
+        // Displaying elements using Iterator
+        System.out.println("Elements in the list:");
+        it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        // Adding elements to the list
+        list.add(5);
+        list.add(10);
+        list.add(15);
+        list.add(20);
+
+        // Displaying elements using Iterator
+        System.out.println("Elements in the list:");
+        it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
         }
 
-        // Adding an element at index 1
-        list.add(1, 25);
-        System.out.println("\nAfter adding element 25 at index 1:");
-        iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-
-        // Checking if the list contains 10
-        System.out.println("\nDoes the list contain 10? " + list.contains(10));
-
-        // Getting the index of element 20
-        System.out.println("Index of 20: " + list.indexOf(20));
-
-        // Getting the size of the list
-        System.out.println("Size of the list: " + list.size());
-
-        // Getting and setting elements
-        System.out.println("Element at index 1: " + list.get(1));
-        list.set(1, 30);
-        System.out.println("Element at index 1 after setting: " + list.get(1));
-
-        // Testing adding elements with iterator
-        System.out.println("\nAdding 100 and 200 with iterator:");
-        iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            int value = iterator.next();
-            if (value == 10) {
-                iterator.add(100);
-                iterator.add(200);
+        // Removing elements from the with iterator
+        it = list.iterator();
+        while (it.hasNext()) {
+            if (it.next() == 10) {
+                it.remove();
             }
         }
-        iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
-
-        // Testing removing elements with iterator
-        System.out.println("\nRemoving elements greater than 100 with iterator:");
-        iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            int value = iterator.next();
-            if (value > 100) {
-                iterator.remove();
-            }
-        }
-        iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
+        // Displaying elements using Iterator
+        System.out.println("Elements in the list:");
+        it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }     
     }
 }
